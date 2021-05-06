b458584bb7dac0d6ad9a7525ba0acb79e94298f1
elaine-caed <77505253+elaine-caed@users.noreply.github.com>
2021-05-06 11:23:14 +0300

    Add files via upload

---
+++ Example_present_4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _4
+{
+	class Program
+	{
+		private static int[] instanceArray(int size, int min, int max)
+		{
+			Random random = new Random();
+			int[] mas = new int[size];
+			for (int i = 0; i < mas.Length; i++)
+			{
+				mas[i] = random.Next(min, max);
+			}
+			return mas;
+		}
+		static void Main(string[] args)
+		{
+			int[] arr = instanceArray(5, 0, 10);
+			foreach (int a in arr)
+			{
+				Console.Write($"{a} ");
+			}
+		}
+	}
+}
---
+++ Example_present_5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = InitArray(4, 5);
+            PrintArray(arr);
+            Console.ReadKey();
+        }
+        private static void PrintArray(int[,] mas)
+        {
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                    Console.Write($"{mas[i, j]} ");
+                Console.WriteLine();
+            }
+        }
+        private static int[,] InitArray(int n, int m)
+        {
+            Random rand = new Random();
+            int[,] mas = new int[n, m];
+            for (int i = 0; i < mas.GetLength(0); i++)
+                for (int j = 0; j < mas.GetLength(1); j++)
+                    mas[i, j] = rand.Next(10, 100);
+            return mas;
+        }
+    }
+}
